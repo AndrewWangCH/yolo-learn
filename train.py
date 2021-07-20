f785011db3be27bc43dacf2d5da26367cac3bb6f
wzl199820 <59794732+wzl199820@users.noreply.github.com>
2021-07-20 18:42:59 +0800

    完善训练脚本

--- train.py
+++ train.py
@@ -6,7 +6,39 @@ import dataset
 import numpy as np
 
 
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def fit_ont_epoch(net, yolo_losses, epoch, train_loader):
+    net.train()
+    for batch_idx, data in enumerate(train_loader, 0):
+        images, target = data[0], data[1]
+        images = images.to(device)
+        target = target.to(device)
+
+        optimizer.zero_grad()
+        outputs = net(images)   # 3个特征图
+        losses = []
+        num_pos_all = 0
+        for i in range(3):
+            loss_item, num_pos = yolo_losses[i](outputs[i], target)
+            losses.append(loss_item)
+            num_pos_all += num_pos
+
+        loss = sum(losses) / num_pos_all
+
+        loss.backward()
+        optimizer.step()
+
+        print('[%d] loss: %.3f' % (epoch + 1, loss.data.item()))
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
 
@@ -24,7 +56,6 @@ if __name__ == "__main__":
     train_loader = dataset.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
 
     net = yolo_v3.YoloBody(yolo_config).to(device)
-    net = net.train()
 
     # 建立yolo函数
     yolo_loss = []
@@ -34,13 +65,12 @@ if __name__ == "__main__":
 
     #------------------------------------------------------#
     #   主干特征提取网络特征通用，冻结训练可以加快训练速度
-    #   也可以在训练初期防止权值被破坏。
-    #   Init_Epoch为起始世代
-    #   Freeze_Epoch为冻结训练的世代
-    #   Unfreeze_Epoch总训练世代
-    #   提示OOM或者显存不足请调小Batch_size
     #------------------------------------------------------#
     lr = 1e-3
     optimizer = torch.optim.Adam(net.parameters(), lr)
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.92)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.92)
+
+    for epoch in range(500):
+        fit_ont_epoch(net, yolo_loss, epoch, train_loader)
+
 
